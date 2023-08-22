c290ab070fa7596d2263c718093ebbefc5c92e66
hluuy <dudwns1289@gmail.com>
2023-08-22 17:42:40 +0900

    update

---
+++ 1991_트리순회.py
@@ -0,0 +1,30 @@
+def pre(root):
+    if root != '.':
+        print(root, end='')
+        pre(tree[root][0])
+        pre(tree[root][1])
+
+def mid(root):
+    if root != '.':
+        mid(tree[root][0])
+        print(root, end='')
+        mid(tree[root][1])
+
+def post(root):
+    if root != '.':
+        post(tree[root][0])
+        post(tree[root][1])
+        print(root, end='')
+
+N = int(input())
+tree = {}
+
+for i in range(N):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+pre('A')
+print()
+mid('A')
+print()
+post('A')
